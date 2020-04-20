a7237e378617c0cae49d5c346992e97eb481d54b
Raveena <raveena.choudhary@gmail.com>
2020-04-20 18:50:30 +0530

    added app.js and base.js

---
+++ pages/App.js
@@ -0,0 +1,9 @@
+class App{
+
+    openHomePage() 
+    {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+    }
+}
+
+export default App
---
+++ pages/Base.js
@@ -0,0 +1,19 @@
+class Base {
+
+     pauseShort()
+     {
+         browser.pause(3000)
+     }
+
+     pauseMedium()
+     {
+         browser.pause(5000)
+     }
+
+     pauseLong()
+     {
+         browser.pause(7000)
+     }
+}
+
+export default Base
